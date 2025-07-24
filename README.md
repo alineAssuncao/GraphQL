39661f315127cfacef3b75603b7c5aa49f42c1f8
alineAssuncao <aline.jassuncao@gmail.com>
2025-07-23 22:51:20 -0300

    atualizações

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # GraphQL
 https://gqlgen.com/
+
+## Schema usado no GraphQL
+
+mutation createCategory {
+  createCategory (input: {name: "Tecnologia", description: "Cursos de Tecnologia"}) {
+    id
+    name
+    description
+  }
+}
+
+mutation createCourse {
+  createCourse (input: {name: "Full Cycle", description: "The best", categoryId: "0c1737fc-19d3-4753-b304-eaab75b8b698"}) {
+    id
+    name
+    description
+  }
+}
+
+query queryCategories{
+  categories {
+    id
+    name
+    description
+  }
+}
+
+query queryCourses{
+  courses {
+    id
+    name
+    description
+  }
+}
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -11,18 +12,55 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/devfullcycle/13-GraphQL/graph"
+	"github.com/devfullcycle/13-GraphQL/internal/database"
 	"github.com/vektah/gqlparser/v2/ast"
+	_ "modernc.org/sqlite" // Import SQLite driver
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS categories (
+			id TEXT PRIMARY KEY,
+			name TEXT NOT NULL,
+			description TEXT
+		)
+	`)
+	if err != nil {
+		log.Fatalf("erro ao criar tabela de categorias: %v", err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS courses (
+			id TEXT PRIMARY KEY,
+			name TEXT NOT NULL,
+			description TEXT,
+			category_id TEXT
+		)
+	`)
+	if err != nil {
+		log.Fatalf("erro ao criar tabela de courses: %v", err)
+	}
+
+	categoryDB := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+		CourseDB:   courseDB,
+	}}))
 
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
